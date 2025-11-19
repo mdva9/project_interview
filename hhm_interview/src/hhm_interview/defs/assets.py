d5cc88d20c85bdf2f35b3b755de6c094a1e231f6
58493 <58493@etu.he2b.be>
2025-11-19 21:00:42 +0100

    extract_raw_products asset done

--- hhm_interview/src/hhm_interview/defs/assets.py
+++ hhm_interview/src/hhm_interview/defs/assets.py
@@ -1,5 +1,13 @@
 from dagster import asset
+import polars as pl
+from pathlib import Path
 
-@asset
-def asset_example():
-    pass
+FILE_PATH = Path(__file__).parent / 'data' / 'products_for_one_day.parquet'
+
+@asset(
+    descritpion="This assets allow to extract the products from the parquet file"
+)
+def extract_raw_products():
+    return pl.read_parquet(FILE_PATH)
+
+
