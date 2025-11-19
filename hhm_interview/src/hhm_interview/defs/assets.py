15be077e4fab40d6dcefb13f05ea878b605e74af
58493 <58493@etu.he2b.be>
2025-11-19 21:13:42 +0100

    wrong_date_type_products asset done

--- hhm_interview/src/hhm_interview/defs/assets.py
+++ hhm_interview/src/hhm_interview/defs/assets.py
@@ -16,3 +16,13 @@ def extract_raw_products() -> pl.DataFrame:
 )
 def no_out_of_stock_products(extract_raw_products:pl.DataFrame) -> pl.DataFrame:
     return extract_raw_products.filter(pl.col("quantity") > 0)
+
+@asset(
+    description="This assets allow to get products where date_type are not good"
+                "Fresh products (F*) should have DLC, dry products (S*) should have DDM."
+)
+def wrong_date_type_products(extract_raw_products:pl.DataFrame) -> pl.DataFrame:
+    return extract_raw_products.filter(
+        (pl.col("boxNumber").str.stars_with("F")& (pl.col("date_type") != "DLC")) |
+        (pl.col("boxNumber").str.stars_with("S") & (pl.col("date_type") != "DDM"))
+    )
